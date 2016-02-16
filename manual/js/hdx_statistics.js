70e0d535c182a52a73425e403f4add6577a28f7f
Luis Capelo <luiscape@gmail.com>
2016-02-16 18:49:08 -0500

    Adding logic for fulling stats from Funnel Space.

---
+++ manual/js/hdx_statistics.js
@@ -0,0 +1,38 @@
+/*
+
+HDXStatiscs class. Used to fetch
+statistics from HDX's statistics service,
+Funnel.Space.
+
+*/
+
+var HDXStatistics = {
+  base: 'http://funnel.space/api/',
+  available_metrics: [],
+  metrics: function (callback) {
+    d3.json(this.base + 'metrics', function (error, data) {
+      if (error) {
+        return error
+      }
+      data.resources.forEach(function (x) { this.HDXStatistics.available_metrics.push(x.metricid) })
+      console.log('Available metrics from Funnel Space:', this.HDXStatistics.available_metrics)
+    })
+  },
+  fetch: function (metricid) {
+    d3.json(this.base + 'funnels?metricid=' + metricid, function (error, data) {
+      if (error) {
+        return error
+      }
+      console.log(data)
+    })
+  },
+  latest: function (metricid, container_id) {
+    d3.json(this.base + 'funnels?metricid=' + metricid, function (error, data) {
+      if (error) {
+        return error
+      }
+      var output = _.maxBy(data.resources, 'period_end_date')
+      document.getElementById(container_id).innerHTML = _.round(output.value, -2)
+    })
+  }
+}
